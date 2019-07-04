e744061934136cab29c9fedea926c85513a84349
saitejach127 <saitejach127@gmail.com>
2019-07-04 22:37:13 +0530

    port fixed

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static(__dirname + '/public'))
 
-app.listen(port, () => { console.log("Server started"); });
+app.listen(PORT, () => { console.log("Server started"); });
