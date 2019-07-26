398cedb6b1ee82c678540577e8734bb5972660dc
saitejach127 <saitejach127@gmail.com>
2019-07-26 16:36:49 +0530

    Folder support

--- public/index.js
+++ public/index.js
@@ -5,6 +5,7 @@ var p = document.getElementById('log');
 var links = document.getElementById('links');
 var downSpeed = document.getElementById('downSpeed');
 var seedFile = document.getElementById('file');
+var seedfolder = document.getElementById('folder');
 let tor;
 
 if(window.location.hash.substring(1)!== ""){
@@ -13,15 +14,15 @@ if(window.location.hash.substring(1)!== ""){
 }
 
 function download(hash) {
-	var data = "magnet:?xt=urn:btih:" + hash + "&dn=name.txt" + "&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com"
+	var data = "magnet:?xt=urn:btih:" + hash + "&dn=name.txt&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969"
 	console.log(data)
 	client.add(data, onTorrent);
 }
 
 function onTorrent(torrent){
+	log("hash = " + torrent.infoHash)
 	tor = torrent;
-	log("hash = " + torrent.infoHash);
-	
+	console.log(torrent)
 	torrent.files.forEach((file) => {
 		file.appendTo('.file');
 	})
@@ -54,4 +55,8 @@ btn.onclick = () => {
 
 seedFile.addEventListener('change',() =>{
 	client.seed(seedFile.files,onTorrent)
+})
+
+seedfolder.addEventListener('change', () => {
+	client.seed(seedfolder.files, onTorrent)
 })
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express()
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 
 app.use(express.static(__dirname + '/public'))
 
